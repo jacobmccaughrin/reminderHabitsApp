76718a1fc590cd2e1dc35909b5e4b799f957cda2
jacobmccaughrin <142435737+jacobmccaughrin@users.noreply.github.com>
2024-02-03 14:04:41 -0800

    notifications

---
+++ notifications.py
@@ -0,0 +1,5 @@
+from functools import partial
+from mac_notifications import client
+
+if __name__ == "__main__":
+    client.create_notification(title="Meeting starts now!", subtitle="Team Standup")
